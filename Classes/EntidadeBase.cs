6aec6dad82ebc44433093a59c060d374c28a1bd0
Leandro Ikehara <leandro.ikehara@gmail.com>
2022-02-25 15:10:18 -0300

    Desafio de Projeto LocalizaLabs

---
+++ Classes/EntidadeBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_de_Projetos_LocalizaLabs
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+    }
+}
